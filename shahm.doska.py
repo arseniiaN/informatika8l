48db950813fea6d283a3884d078bf012141f75db
Арсения <148338074+arseniiaN@users.noreply.github.com>
2023-12-05 21:23:35 +0400

    update shahm.doska.py

---
+++ shahm.doska.py
@@ -0,0 +1,8 @@
+y1 = int(input('y1 = '))
+x1 = int(input('x1 = '))
+y2 = int(input('y2 = '))
+x2 = int(input('x2 = '))
+if((y1 == x1) and (y2 == x2) and (y1,x1,y2,x2 < 8)):
+    print("yes")
+else:
+    print("no")
